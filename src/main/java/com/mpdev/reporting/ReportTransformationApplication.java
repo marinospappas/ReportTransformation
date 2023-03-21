6a23bdd3b7440872b9bbb47e273f9f1be989095c
marinos <marinosch.99981@gmail.com>
2023-03-21 13:51:04 +0100

    First commit

---
+++ src/main/java/com/mpdev/reporting/ReportTransformationApplication.java
@@ -0,0 +1,12 @@
+package com.mpdev.reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportTransformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportTransformationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mpdev/reporting/ReportTransformationApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mpdev.reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportTransformationApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
