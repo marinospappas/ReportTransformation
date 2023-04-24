b34264cca3d6f2161f55fd5421ded1267901c42a
marinos <marinosch.99981@gmail.com>
2023-04-24 12:21:11 +0200

    refactored BatchConfiguration

--- src/main/java/com/mpdev/reporting/config/BatchConfiguration.java
+++ src/main/java/com/mpdev/reporting/config/BatchConfiguration.java
@@ -3,13 +3,13 @@ package com.mpdev.reporting.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mpdev.reporting.report.inreport.InputItem;
 import com.mpdev.reporting.report.outreport.OutputItem;
-import com.mpdev.reporting.processor.InputItemProcessor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -26,10 +26,30 @@ import javax.sql.DataSource;
 public class BatchConfiguration {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final InputItemProcessor inputItemProcessor;
 
-    public BatchConfiguration(InputItemProcessor inputItemProcessor) {
-        this.inputItemProcessor = inputItemProcessor;
+    @Bean
+    public Job importUserJob(JobRepository jobRepository,
+                             JobCompletionNotificationListener listener, Step batchStep) {
+        return new JobBuilder("transformReport", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(batchStep)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step batchStep(JobRepository jobRepository,
+                      PlatformTransactionManager transactionManager,
+                      FlatFileItemReader<InputItem> reader,
+                      ItemProcessor<InputItem,OutputItem> itemProcessor,
+                      JdbcBatchItemWriter<OutputItem> writer) {
+        return new StepBuilder("batchStep1", jobRepository)
+                .<InputItem, OutputItem> chunk(10, transactionManager)
+                .reader(reader)
+                .processor(itemProcessor)
+                .writer(writer)
+                .build();
     }
 
     @Bean
@@ -42,7 +62,7 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public JdbcBatchItemWriter<OutputItem> writer(DataSource dataSource) {
+    public JdbcBatchItemWriter<OutputItem> writerJdbc(DataSource dataSource) {
         return new JdbcBatchItemWriterBuilder<OutputItem>()
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
                 .sql("INSERT INTO report (contract_id, last_name, first_name, item_type, jurisdiction, end_date) " +
@@ -50,27 +70,4 @@ public class BatchConfiguration {
                 .dataSource(dataSource)
                 .build();
     }
-
-    @Bean
-    public Job importUserJob(JobRepository jobRepository,
-                             JobCompletionNotificationListener listener, Step step1) {
-        return new JobBuilder("transformReport", jobRepository)
-                .incrementer(new RunIdIncrementer())
-                .listener(listener)
-                .flow(step1)
-                .end()
-                .build();
-    }
-
-    @Bean
-    public Step step1(JobRepository jobRepository,
-                     PlatformTransactionManager transactionManager, JdbcBatchItemWriter<OutputItem> writer) {
-        return new StepBuilder("step1", jobRepository)
-                .<InputItem, OutputItem> chunk(10, transactionManager)
-                .reader(readerJson())
-                .processor(inputItemProcessor)
-                .writer(writer)
-                .build();
-    }
-
 }
