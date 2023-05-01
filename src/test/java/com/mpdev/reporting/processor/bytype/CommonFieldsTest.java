8d1b67aca98fdb0f8639a703c6fd0c92f107b042
marinos <marinosch.99981@gmail.com>
2023-05-01 19:12:29 +0200

    added CommonFieldsTest for enrichment of fields commons across all record types

--- src/test/java/com/mpdev/reporting/processor/bytype/CommonFieldsTest.java
+++ src/test/java/com/mpdev/reporting/processor/bytype/CommonFieldsTest.java
@@ -1,19 +1,19 @@
 package com.mpdev.reporting.processor.bytype;
 
 import com.mpdev.reporting.processor.Transformer;
-import com.mpdev.reporting.transformation.ReportTranformation;
 import com.mpdev.reporting.report.inreport.InputItem;
+import com.mpdev.reporting.transformation.ReportTranformation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class PublicProcessorTest {
+public class CommonFieldsTest {
 
     private InputItem inputItem;
     private PublicItemStrategy publicItemStrategy;
@@ -26,13 +26,16 @@ public class PublicProcessorTest {
         publicItemStrategy = new PublicItemStrategy(reportTranformation);
     }
 
-    @Test
-    @DisplayName("Public items fields are transformed correctly")
-    void testPublicItemFields() {
+    @ParameterizedTest
+    @CsvSource(nullValues = "null", value = {
+            "2024-09-01, UK, 01/09/2024",
+            "2024-09-01, EU, 01/09/2024",
+    })
+    @DisplayName("EndDate field is enriched according to Jurisdiction")
+    void testEndDate(String endDate, String jurisdiction, String expected) {
+        inputItem.setEndDate(endDate);
+        inputItem.setJurisdiction(jurisdiction);
         var output = publicItemStrategy.apply(inputItem);
-        assertEquals(inputItem.getFirstName().toUpperCase(), output.getFirstName());
-        assertEquals(inputItem.getLastName().toUpperCase(), output.getLastName());
-        assertEquals(inputItem.getJurisdiction().toUpperCase(), output.getJurisdiction());
+        assertEquals(expected, output.getEndDate());
     }
-
 }
