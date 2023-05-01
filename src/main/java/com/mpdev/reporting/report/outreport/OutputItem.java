0d0a913911e3a421a4c8946f4d514e9174ce4113
marinos <marinosch.99981@gmail.com>
2023-05-01 19:17:48 +0200

    added CommonFieldsTest for enrichment of fields commons across all record types

--- src/main/java/com/mpdev/reporting/report/outreport/OutputItem.java
+++ src/main/java/com/mpdev/reporting/report/outreport/OutputItem.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -27,7 +26,6 @@ public class OutputItem {
             groups = {ConfidentialRecord.class, SecretRecord.class})
     private String lastName;
     private String firstName;
-    @Pattern(regexp = "^[PCS]$", message = "EX001: Invalid Record Type")
     private String itemType;
     private String jurisdiction;
     private String endDate;
